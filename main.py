effc336741685545a59bdd5b6faa355902fa314c
Brian O'Kelly <brianjrok@gmail.com>
2022-11-03 18:58:34 -0600

    function to check for money

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 # TODO: 2. Check resources sufficient to make drink order.
-def checkResources():
-    for drink in MENU:
-        can_make = True
+def checkResources(drink):
+    if drink in MENU:
         for ingredient in MENU[drink]["ingredients"]:
             if resources[ingredient] < MENU[drink]["ingredients"][ingredient]:
-                can_make = False
-        print("There are enough resources to make " + drink) if can_make else print("There are not enough resources to make" + drink)
+                print("Sorry there is not enough " + ingredient + ".")
+                return
+        checkMoney(drink)
+    else:
+        print("Can't find that drink")
 
+def checkMoney(drink):
+    total = 0
+    change = input("Please enter $" + str('${:,.2f}'.format(MENU[drink]["cost"])) + " in change.\n[quarters, dimes, nickles, pennies]\n")
+    for coin in change.split(","):
+        print(coin)
+        # TODO 1. Need to fix indent at beggning of string
+    #     coin_type = change.rstrip('0123456789')
+    #     coin_quantity = change[len(coin_type):]
+    #     total += (coins[coin_type] * int(coin_quantity))
+    # print(total)
+
+coins = {
+    "q": 25,
+    "d": 10,
+    "n": 5,
+    "p": 1
+}
 
 MENU = {
     "espresso": {
@@ -42,8 +61,20 @@ resources = {
 
 # TODO: 1. Print report of all the coffee machine resources.
 def printResources():
-    print("Water: " + str(resources["water"]))
-    print("MilK: " + str(resources["milk"]))
-    print("Coffee: " + str(resources["coffee"]))
+    print()
+    print("Water: " + str(resources["water"]) + "ml")
+    print("MilK: " + str(resources["milk"]) + "ml")
+    print("Coffee: " + str(resources["coffee"]) + "g")
+    print("Money")
 
-checkResources()
+def start():
+    response = input("What would you like? (espresso/latte/cappuccino):\n")
+    match response:
+        case "off":
+            return
+        case "report":
+            printResources()
+        case _:
+            checkResources(response)
+
+start()
